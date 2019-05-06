0bce7d4750b728cf0b571e36fdb67282e40efc2c
Hazmi <whitespaces@gmail.com>
2019-05-07 05:02:46 +0700

    next.js hello world

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import "./index.scss";
+
+const Index = () => {
+  return (
+    <div className="home">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default Index;
