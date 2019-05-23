7fd55444f7ac80059813a9407262b68a843b4df8
Hazmi <whitespaces@gmail.com>
2019-05-23 14:46:56 +0700

    add offline support + add typescripts types

--- next.config.js
+++ next.config.js
@@ -1,3 +1,29 @@
 const withSass = require('@zeit/next-sass')
 const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript(withSass())
+const withOffline = require('next-offline')
+
+const nextConfig = {
+  target: "serverless",
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'networkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+}
+
+module.exports = withTypescript(withOffline(withSass(nextConfig)))
