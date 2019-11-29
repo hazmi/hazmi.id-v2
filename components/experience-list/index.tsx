d9bf8f708ea40fa70c8eed55a9ddefc464be8eca
Hazmi <whitespaces@gmail.com>
2019-11-30 02:02:20 +0700

    update experience list

--- components/experience-list/index.tsx
+++ components/experience-list/index.tsx
@@ -5,10 +5,10 @@ export const ExperienceList = () => {
   return (
     <div className="experience-list">
       <section className="experience-list__item">
-        <img src="/static/work/gojek.png" alt="Logo Gojek" />
+        <img src="/static/work/gojek.png" alt="Logo Gojek" width="50" />
         <div className="experience-list__item-content">
           <h5 className="experience-list__item-date">Aug 2019 &mdash; Current</h5>
-          <h3 className="experience-list__item-title">Product Engineer</h3>
+          <h3 className="experience-list__item-title">Frontend Engineer Lead</h3>
           <h4 className="experience-list__item-company"><span>at</span> Gojek</h4>
           <div className="experience-list__item-info">
             <p>Working on websites.</p>
@@ -16,9 +16,9 @@ export const ExperienceList = () => {
         </div>
       </section>
       <section className="experience-list__item">
-        <img src="/static/work/mango33.png" alt="Logo Mango33" />
+        <img src="/static/work/mango33.png" alt="Logo Mango33" width="50" />
         <div className="experience-list__item-content">
-          <h5 className="experience-list__item-date">May 2018 &mdash; Aug 2019</h5>
+          <h5 className="experience-list__item-date">May 2018 &mdash; Aug 2019 &middot; 1yr 4mos</h5>
           <h3 className="experience-list__item-title">Tech Lead / IT Director</h3>
           <h4 className="experience-list__item-company"><span>at</span> Mango33 Pte Ltd</h4>
           <div className="experience-list__item-info">
@@ -27,7 +27,7 @@ export const ExperienceList = () => {
         </div>
       </section>
       <section className="experience-list__item">
-        <img src="/static/work/cellihealth.png" alt="Logo CelliHealth" />
+        <img src="/static/work/cellihealth.png" alt="Logo CelliHealth" width="50" />
         <div className="experience-list__item-content">
           <h5 className="experience-list__item-date">May 2016 &mdash; May 2018 &middot; 2yrs 1mo</h5>
           <h3 className="experience-list__item-title">Tech Lead / IT Director</h3>
@@ -38,7 +38,7 @@ export const ExperienceList = () => {
         </div>
       </section>
       <section className="experience-list__item">
-        <img src="/static/work/kmk.png" alt="Logo KMK" />
+        <img src="/static/work/kmk.png" alt="Logo KMK" width="50" />
         <div className="experience-list__item-content">
           <h5 className="experience-list__item-date">May 2015 &mdash; May 2016 &middot; 1yr 1mo</h5>
           <h3 className="experience-list__item-title">Lead Front-end Engineer</h3>
@@ -49,7 +49,7 @@ export const ExperienceList = () => {
         </div>
       </section>
       <section className="experience-list__item">
-        <img src="/static/work/graipe.png" alt="Logo Graipe Labs" />
+        <img src="/static/work/graipe.png" alt="Logo Graipe Labs" width="50" />
         <div className="experience-list__item-content">
           <h5 className="experience-list__item-date">Jan 2013 &mdash; Apr 2015 &middot; 2yr 3mos</h5>
           <h3 className="experience-list__item-title">Tech Lead / VP Technology</h3>
@@ -60,7 +60,7 @@ export const ExperienceList = () => {
         </div>
       </section>
       <section className="experience-list__item">
-        <img src="/static/work/kitook.png" alt="Logo Kitook" />
+        <img src="/static/work/kitook.png" alt="Logo Kitook" width="50" />
         <div className="experience-list__item-content">
           <h5 className="experience-list__item-date">Aug 2012 &mdash; Apr 2014 &middot; 1yr 9mos</h5>
           <h3 className="experience-list__item-title">CTO</h3>
@@ -71,7 +71,7 @@ export const ExperienceList = () => {
         </div>
       </section>
       <section className="experience-list__item">
-        <img src="/static/work/gonla.png" alt="Logo Gonla" />
+        <img src="/static/work/gonla.png" alt="Logo Gonla" width="50" />
         <div className="experience-list__item-content">
           <h5 className="experience-list__item-date">Oct 2011 &mdash; Aug 2012 &middot; 11mos</h5>
           <h3 className="experience-list__item-title">Senior Web Developer</h3>
@@ -82,7 +82,7 @@ export const ExperienceList = () => {
         </div>
       </section>
       <section className="experience-list__item">
-        <img src="/static/work/xhtmlized.png" alt="Logo XHTMLized" />
+        <img src="/static/work/xhtmlized.png" alt="Logo XHTMLized" width="50" />
         <div className="experience-list__item-content">
           <h5 className="experience-list__item-date">Oct 2007 &mdash; Aug 2011 &middot; 4yrs 4mos</h5>
           <h3 className="experience-list__item-title">Front-end Web Developer</h3>
@@ -93,7 +93,7 @@ export const ExperienceList = () => {
         </div>
       </section>
       <section className="experience-list__item">
-        <img src="/static/work/agrisoft.png" alt="Logo Agrisoft" />
+        <img src="/static/work/agrisoft.png" alt="Logo Agrisoft" width="50" />
         <div className="experience-list__item-content">
           <h5 className="experience-list__item-date">Jan 2005 &mdash; Jun 2007 &middot; 2yrs 6mos</h5>
           <h3 className="experience-list__item-title">Web Developer</h3>
