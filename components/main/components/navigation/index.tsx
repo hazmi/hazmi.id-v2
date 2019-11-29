94e52e552944538ab6179b8e87fa1c126ef9d0ce
Hazmi <whitespaces@gmail.com>
2019-11-30 02:02:57 +0700

    update nav to use list

--- components/main/components/navigation/index.tsx
+++ components/main/components/navigation/index.tsx
@@ -6,29 +6,33 @@ const className = "navigation__item"
 export const Navigation = ({ data, router }) => {
   let activeNav = 0;
   return (
-    <div className="navigation">
-      {data.map(item => {
-        const curClassName: string = router.pathname === item.link ? `${className} ${className}_active` : className;
-        if (activeNav === 0 && router.pathname === item.link) {
-          activeNav = item.id;
-        }
-        return (
-          <a
-            key={item.link}
-            href={item.link}
-            aria-label={`Go to ${item.title} page`}
-            onClick={e => {
-              e.preventDefault();
-              router.push(item.link);
-            }}
-            className={curClassName}
-          >
-            <item.icon />
-          </a>
-        )
-      })}
-      <span className={`navigation__active-link navigation__active-link_${activeNav}`} />
-    </div>
+    <nav className="navigation">
+      <ul>
+        {data.map(item => {
+          const curClassName: string = router.pathname === item.link ? `${className} ${className}_active` : className;
+          if (activeNav === 0 && router.pathname === item.link) {
+            activeNav = item.id;
+          }
+          return (
+            <li key={item.link}>
+              <a
+                href={item.link}
+                aria-label={`Go to ${item.title} page`}
+                onClick={e => {
+                  e.preventDefault();
+                  router.push(item.link);
+                }}
+                className={curClassName}
+              >
+                <item.icon />
+                <span>{item.title}</span>
+              </a>
+            </li>
+          )
+        })}
+        <span className={`navigation__active-link navigation__active-link_${activeNav}`} />
+      </ul>
+    </nav>
   )
 };
 
